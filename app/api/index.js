0e10bd5ee2625cf2ca310ca96c492c2b6ec3d859
george1605 <george160505@gmail.com>
2022-09-19 20:24:36 +0300

    Another try!

---
+++ app/api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    try {
+        res.json({"Why":"This?"});
+    }catch(e)
+    {
+        console.log(e);
+        res.status(500).send("Server Error!");
+    }
+})
+
+module.exports = router;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 // this will deploy the entire dir 
 const express = require('express');
 const path = require('path');
+const api = require('./api/index');
 const app = express();
 
 app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public'))
+app.use("/api/*", api);
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 })
