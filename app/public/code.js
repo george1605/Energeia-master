0414ee296d51d404dd411605e809f43ea9f9da52
george1605 <george160505@gmail.com>
2022-10-06 19:12:34 +0300

    tryed

--- app/public/code.js
+++ app/public/code.js
@@ -7,17 +7,15 @@ function preventXSS(input)
     return input.replace("<", "&lt;").replace(">", "&gt;");
 }
 
-function addNewsletter()
+function addNewsletter(e)
 {
-    document.querySelector("#newsletter").addEventListener('keypress', (e) => {
-        if(e.code == 13)
-        {
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/form", true);
-            xhr.onreadystatechange = () => {
-                console.log("XHR Newsletter Status: ", xhr.status);
-            }
-            xhr.send(preventXSS(e.target.value));
+    if(e.code == 13)
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/form", true);
+        xhr.onreadystatechange = () => {
+            console.log("XHR Newsletter Status: ", xhr.status);
         }
-    })
+        xhr.send(preventXSS(e.target.value));
+    }
 }
